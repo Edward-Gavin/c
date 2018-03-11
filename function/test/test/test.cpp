a40466eb809d2015038904e7faa612ae5a3be876
Gavin <wsw_it_3325@163.com>
2018-03-11 19:24:21 +0800

    add function test program.

--- function/test/test/test.cpp
+++ function/test/test/test.cpp
@@ -3,7 +3,7 @@
 #include "string.h"
 #include "math.h"
 
-int main(void)
+int main1(void)
 {
 	void print_star();
 	void print_message();
@@ -26,3 +26,27 @@ void print_message()
 {
 	printf("How do you do! \n");
 }
+
+
+int max(int a, int b)
+{
+	int z;
+	z = (a > b) ? a : b;
+	return z;
+}
+
+int main2(void)
+{
+	int max(int, int);
+
+	int temp, a, b, c, maxnum;
+	printf("Please input three numbers: \n");
+	scanf_s("%d,%d,%d", &a, &b, &c);
+	temp = max(a, b);
+	maxnum = max(temp, c);
+	printf("The max number is %d", maxnum);
+
+	system("pause");
+	return 0;
+}
+
