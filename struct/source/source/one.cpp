8b4d5b964b707128927174cfade4604f3e5f6ffe
Gavin <wsw_it_3325@163.com>
2018-03-19 23:04:38 +0800

    add struct test.

--- struct/source/source/one.cpp
+++ struct/source/source/one.cpp
@@ -2,7 +2,7 @@
 #include "stdlib.h"
 #include "string.h"
 
-int main(void)
+int main1(void)
 {
 	struct Date {
 		int day;
@@ -28,4 +28,57 @@ int main(void)
 
 	system("pause");
 	return 0;
+}
+
+int main2(void)
+{
+	struct Student
+	{
+		long num;
+		char name[10];
+		char sex;
+		float score;
+	};
+	struct Student stu_1;
+	struct Student *p;
+
+	p = &stu_1;
+	stu_1.num = 10101;
+	strcpy_s(stu_1.name, "lilin");
+	stu_1.sex = 'M';
+	stu_1.score = 89.5;
+
+	printf("No. %ld\nName: %s\nSex: %c\nScore: %5.1f\n", stu_1.num, stu_1.name, stu_1.sex, stu_1.score);
+	printf("No. %ld\nName: %s\nSex: %c\nScore: %5.1f\n", (*p).num, (*p).name, (*p).sex, (*p).score);
+	printf("No. %ld\nName: %s\nSex: %c\nScore: %5.1f\n", p->num, p->name, p->sex, p->score);
+	
+	
+	system("pause");
+	return 0;
+}
+
+int main3(void)
+{
+	struct Student {
+		int num;
+		char name[10];
+		char sex;
+		int age;
+	};
+
+	struct Student stu[3] = {
+		{10101,"liming",'M', 20},{10102, "lilei",'M', 21},{10103, "hanmeimei",'F', 22}
+	};
+	struct Student *p;
+	printf("No.	name		sex	age\n");
+	for (p = stu; p < stu + 3; p++)
+		printf("%5d %-20s %2c %4d\n", p->num, p->name, p->sex, p->age);
+
+	system("pause");
+	return 0;
+}
+
+int main(void)
+{
+
 }
