9f3bf4d0e57f024d6292d121ac44237106fb2bd0
Gavin <wsw_it_3325@163.com>
2018-03-22 17:47:48 +0800

    add test for chapter 7.

--- question/7/test/test/1.cpp
+++ question/7/test/test/1.cpp
@@ -44,7 +44,7 @@ int lcd(int u, int v, int h)
 
 float x1, x2, disc, p, q;
 
-int main(void)
+int main2(void)
 {
 	void great_than_zero(float, float);
 	void equal_to_zero(float, float);
@@ -95,3 +95,67 @@ void smaller_than_zero(float a, float b)
 	q = sqrt(-disc) / (2 * a);
 }
 
+
+int main3(void)
+{
+	int prime(int);
+
+	int n;
+	printf("Input an integer: ");
+	scanf_s("%d", &n);
+	if (prime(n))
+		printf("%d is a prime.\n", n);
+	else
+		printf("%d is not a prime. \n");
+
+	system("pause");
+	return 0;
+}
+
+int prime(int n)
+{
+	int flag = 1, i;
+	for (i = 2; i < n / 2 && flag == 1; i++)
+		if (n%i == 0)
+			flag = 0;
+	return (flag);
+}
+
+int array[3][3];
+int main(void)
+{
+
+	void convert(int array[][3]);
+
+	int i, j;
+	printf("Input array: \n");
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			scanf_s("%d", &array[i][j]);
+	printf("\noriginal array: \n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			printf("%5d", array[i][j]);
+		printf("\n");
+	}
+	convert(array);
+	printf("convert array :\n");
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+			printf("%5d", array[i][j]);
+		printf("\n");
+	}
+
+
+	system("pause");
+	return 0;
+}
+
+
+void convert(int array[][3])
+{
+	int i, j, k;
+	for(i=0; i<3;i++)
+}
